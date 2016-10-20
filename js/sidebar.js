d019ba30b873e60cacc712be79bcc41246625aea
harley-travis <harley.travis2@gmail.com>
2016-10-20 12:14:08 -0600

    unbeautifed

--- js/sidebar.js
+++ js/sidebar.js
@@ -11,5 +11,23 @@
     **
 **/
 
-$(document).ready(function(){var a="",b=location.origin+"/peakalarm/index.php/services/",d=(location.origin+"/peakalarm/index.php/products/","/peakalarm/index.php/peak-link/"),e="/peakalarm/index.php/peak-view/",f="/peakalarm/index.php/peak-edge/";$(function(){(window.location.href==location.origin+d||window.location.href==location.origin+e||window.location.href==location.origin+f)&&$(".currentProducts").addClass("current")}),$(".parent a").each(function(){-1!=window.location.href.indexOf($(this).prop("href"))&&($(this).addClass("current"),$(this).closest(".parent").find("ul.subnav").slideDown())}),$(".subnav-list a").each(function(){-1!=window.location.href.indexOf($(this).prop("href"))&&($(this).addClass("current"),$(this).closest(".parent").find(".parentInt").attr("href")==a?$(".currentAbout").addClass("current"):$(this).closest(".parent").find(".parentInt").attr("href")!=b&&$(".currentServices").addClass("current"))}),$(function(){$("a").each(function(){$(this).prop("href")==window.location.href&&$(this).addClass("current")})}),$("li.parent").click(function(){$(this).siblings().find(".subnav").slideUp(),$(this).children(".subnav").slideDown()})});
-
+$(document).ready(function() {
+    var a = "",
+        b = location.origin + "/peakalarm/index.php/services/",
+        d = (location.origin + "/peakalarm/index.php/products/", "/peakalarm/index.php/peak-link/"),
+        e = "/peakalarm/index.php/peak-view/",
+        f = "/peakalarm/index.php/peak-edge/";
+    $(function() {
+        (window.location.href == location.origin + d || window.location.href == location.origin + e || window.location.href == location.origin + f) && $(".currentProducts").addClass("current")
+    }), $(".parent a").each(function() {
+        -1 != window.location.href.indexOf($(this).prop("href")) && ($(this).addClass("current"), $(this).closest(".parent").find("ul.subnav").slideDown())
+    }), $(".subnav-list a").each(function() {
+        -1 != window.location.href.indexOf($(this).prop("href")) && ($(this).addClass("current"), $(this).closest(".parent").find(".parentInt").attr("href") == a ? $(".currentAbout").addClass("current") : $(this).closest(".parent").find(".parentInt").attr("href") != b && $(".currentServices").addClass("current"))
+    }), $(function() {
+        $("a").each(function() {
+            $(this).prop("href") == window.location.href && $(this).addClass("current")
+        })
+    }), $("li.parent").click(function() {
+        $(this).siblings().find(".subnav").slideUp(), $(this).children(".subnav").slideDown()
+    })
+});
